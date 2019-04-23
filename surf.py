81a14db20993b1ee0a80f85b9346343607eee03a
Elio Champenois <elio.champenois@gmail.com>
2019-04-23 16:15:48 -0700

    v1
    updated version that has been used at beamtimes, etc.
    - input checks and rotation
    - log option
    - colorbar option
    - kwargs for pcolormesh

--- surf.py
+++ surf.py
@@ -1,12 +1,37 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from matplotlib.pyplot import pcolormesh, xlim, ylim, colorbar as cb
+from matplotlib.colors import LogNorm
+from numpy import concatenate, diff, vstack, hstack, zeros, isfinite
 
-def surf(x, y, z):
+def surf(x, y, z, colorbar=True, log=False, min_over_max=1e-30, **kwargs):
 
-	x = np.concatenate(([(3*x[0]-x[1])/2.],x[:-1]+np.diff(x)/2.,[(3*x[-1]-x[-2])/2.]))
-	y = np.concatenate(([(3*y[0]-y[1])/2.],y[:-1]+np.diff(y)/2.,[(3*y[-1]-y[-2])/2.]))
-	z = np.vstack((np.hstack((z,np.zeros((z.shape[0],1)))),np.zeros((1,z.shape[1]+1))))
-	plt.pcolormesh(x, y, z)
-	plt.xlim(x[[0,-1]])
-	plt.ylim(y[[0,-1]])
-	plt.colorbar()
+	lx, ly, sz = len(x), len(y), z.shape
+
+	if len(sz)==3:
+		if sz[3]==1:
+			z = z[:,:,0]
+		else:
+			raise ValueError('z must be 2-D')
+
+	if lx==sz[0] and ly==sz[1]:
+		x, y = y, x
+	else:
+		assert lx==sz[1] and ly==sz[0], 'x, y, and z array sizes do not match'
+
+	x = concatenate(([(3*x[0]-x[1])/2.],x[:-1]+diff(x)/2.,[(3*x[-1]-x[-2])/2.]))
+	y = concatenate(([(3*y[0]-y[1])/2.],y[:-1]+diff(y)/2.,[(3*y[-1]-y[-2])/2.]))
+	z = vstack((hstack((z,zeros((z.shape[0],1)))),zeros((1,z.shape[1]+1))))
+    
+	if log:
+		zvals = z[(z>0)&isfinite(z)]
+		vmax = zvals.max()
+		vmin = max(zvals.min(), vmax*min_over_max)
+		norm = LogNorm(vmin=vmin, vmax=vmax)
+	else:
+		norm=None
+    
+	plot = pcolormesh(x, y, z, norm=norm, **kwargs)
+	xlim(x[[0,-1]])
+	ylim(y[[0,-1]])
+	if colorbar:
+		cb()
+	return plot
